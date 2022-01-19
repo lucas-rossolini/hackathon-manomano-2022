59e8d944dc7a8e275c1fbb55015587f86ce0802a
Camille Hautois <camille.hautois@gmail.com>
2022-01-19 18:30:54 +0100

    Remake ManoMano footer without clickables links

--- manomano-app/src/App.jsx
+++ manomano-app/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
-import { Home, Page404 } from "pages";
+import { Home, Page404 } from "./pages";
 
 import "./App.scss";
 
