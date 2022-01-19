5b357774d8a1c11241fb84168bd1379de4a3756c
Vianney-Gln <vgeloen@gmail.com>
2022-01-19 19:45:07 +0100

    creation de data pour salle de bain 1ere partie

--- manomano-app/src/data/productsData.jsx
+++ manomano-app/src/data/productsData.jsx
@@ -16,6 +16,76 @@ const productsData = [
   // Fin Lucas ----------------------------------
 
   // Vianney ------------------------------------
+  {
+    // vasque
+    name: "Bathroom Vanity Wash Basin Sink Countertop Oval Curved White Modern 410 x 330mm",
+    link: "",
+    img: "https://cdn.manomano.com/bathroom-vanity-wash-basin-sink-countertop-oval-curved-white-modern-410-x-330mm-P-4029359-12024024_1.jpg",
+    brandName: "AFFINE",
+    logo: "https://cdn.manomano.com/brand-affine-261158.png",
+    rate: 5,
+    rateNumber: 21,
+    priceInt: 39,
+    priceDec: 57,
+    freeDelivery: true,
+    keywords: ["interior"],
+  },
+  {
+    // douche
+    name: "Bathroom Thermostatic Mixer Shower Set Square Chrome Twin Head Exposed Valve",
+    link: "https://www.manomano.co.uk/p/thermostatic-mixer-shower-set-square-chrome-twin-head-exposed-8455999",
+    img: "https://cdn.manomano.com/images/images_products/4029359/L/8756408_1.jpg",
+    brandName: "ARCHITEKT",
+    logo: "https://cdn.manomano.com/brand-architeckt-261164.png",
+    rate: 4.5,
+    rateNumber: 204,
+    priceInt: 69,
+    priceDec: 97,
+    freeDelivery: true,
+    keywords: ["interior"],
+  },
+  {
+    // faillence
+    name: "Mosaic tile mirror kitchen backsplash and bathroom Brillo Gris",
+    link: "https://www.manomano.co.uk/p/mosaic-tile-mirror-kitchen-backsplash-and-bathroom-brillo-gris-34753785",
+    img: "https://cdn.manomano.com/mosaic-tile-mirror-kitchen-backsplash-and-bathroom-brillo-gris-P-833994-33300596_1.jpg",
+    brandName: "ARCHITEKT",
+    logo: "https://cdn.manomano.com/brand-architeckt-261164.png",
+    rate: "",
+    rateNumber: "",
+    priceInt: 14,
+    priceDec: 88,
+    freeDelivery: true,
+    keywords: ["interior"],
+  },
+  {
+    // mirroir
+    name: "ELEGANT 800 x 500mm Backlit LED Illuminated Bathroom Mirror with Light Sensor + Demister",
+    link: "https://www.manomano.co.uk/p/elegant-800-x-500mm-backlit-led-illuminated-bathroom-mirror-with-light-sensor-demister-12003754",
+    img: "https://cdn.manomano.com/elegant-800-x-500mm-backlit-led-illuminated-bathroom-mirror-with-light-sensor-demister-P-6078297-12324289_1.jpg",
+    brandName: "ELEGANT",
+    logo: "https://cdn.manomano.com/brand-elegant-116460.jpg",
+    rate: 4.5,
+    rateNumber: 71,
+    priceInt: 91,
+    priceDec: 99,
+    freeDelivery: true,
+    keywords: ["interior"],
+  },
+  {
+    // carrelage
+    name: "Floor Tiles Self Adhesive Grey White Geo Vinyl Flooring Kitchen Bathroom 1m",
+    link: "https://www.manomano.co.uk/catalogue/p/floor-tiles-self-adhesive-grey-white-geo-vinyl-flooring-kitchen-bathroom-1m-36157197",
+    img: "https://cdn.manomano.com/floor-tiles-self-adhesive-grey-white-geo-vinyl-flooring-kitchen-bathroom-1m-P-1504056-34741270_1.jpg",
+    brandName: "",
+    logo: "https://cdn.manomano.com/brand-elegant-116460.jpg",
+    rate: "",
+    rateNumber: "",
+    priceInt: 15,
+    priceDec: 99,
+    freeDelivery: true,
+    keywords: ["interior"],
+  },
 
   // Fin Vianney --------------------------------
 
