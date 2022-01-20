29c6e035da74a8473405dccfa1b9ba5da4df8851
maxime baillon <baillonmaximebaillon@gmail.com>
2022-01-20 15:11:29 +0100

    shopping cart component added in components

--- manomano-app/src/App.jsx
+++ manomano-app/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home, Page404 } from "./pages";
 import "./App.scss";
+import Basket from "./components/Basket";
 // import Form from "./components/Form";
 
 function App() {
   return (
     <div>
+      <Basket />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="*" element={<Page404 />} />
